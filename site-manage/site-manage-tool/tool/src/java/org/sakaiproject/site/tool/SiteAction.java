dfd89b7006eaf49c01f98a76c7daec5427e750da
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2015-02-23 08:06:43 +0000

    SU-78 Show section ID on the site settings page.
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.4/trunk@317708 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4330,6 +4330,10 @@ public class SiteAction extends PagedResourceActionII {
 				}
 			}
 			context.put("providerCourseTitles", sectionTitles);
+
+                        // CLASSES-455 NYU Change - sort course list by id
+                        Collections.sort(providerCourseList);
+
 			context.put("providerCourseList", providerCourseList);
 		}
 
