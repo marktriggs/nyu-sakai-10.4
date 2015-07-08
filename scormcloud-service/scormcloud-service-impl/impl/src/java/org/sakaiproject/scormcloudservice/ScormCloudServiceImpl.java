4e6911fcd2fd86ed8937b049041a509ace08a94e
Mark Triggs <mark@dishevelled.net>
2015-07-08 15:31:50 +1000

    More bits

--- scormcloud-service/scormcloud-service-impl/impl/src/java/org/sakaiproject/scormcloudservice/ScormCloudServiceImpl.java
+++ scormcloud-service/scormcloud-service-impl/impl/src/java/org/sakaiproject/scormcloudservice/ScormCloudServiceImpl.java
@@ -3,6 +3,11 @@ package org.sakaiproject.scormcloudservice.impl;
 import org.sakaiproject.scormcloudservice.api.ScormException;
 import org.sakaiproject.scormcloudservice.api.ScormCloudService;
 
+import com.rusticisoftware.hostedengine.client.Configuration;
+import com.rusticisoftware.hostedengine.client.CourseService;
+import com.rusticisoftware.hostedengine.client.ScormCloud;
+
+
 class ScormCloudServiceImpl implements ScormCloudService {
 
     public String getScormPlayerUrl(String externalId) {
@@ -14,6 +19,17 @@ class ScormCloudServiceImpl implements ScormCloudService {
     }
 
     public void init() {
+        Configuration config = new Configuration("http://thweeble:2345", "poop", "poop", "nyuclasses.10.4");
+        ScormCloud.setConfiguration(config);
+
+        try {
+            CourseService service = ScormCloud.getCourseService();
+
+            // service
+            System.err.println("\n*** DEBUG " + System.currentTimeMillis() + "[ScormCloudServiceImpl.java:26 f6c163]: " + "\n    service => " + (service) + "\n");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void destroy() {
